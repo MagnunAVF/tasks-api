96bcea4004c8ef5305f62179a824707ecfd3841a
Magnun A V F <magnunavf@gmail.com>
2023-05-14 01:28:29 -0300

    Create health check route

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/MagnunAVF/tasks-api/handlers"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	router := gin.Default()
+
+	router.GET("/", handlers.HealthHandler)
+
+	router.Run(":8080")
 }
