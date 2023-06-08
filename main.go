083b0f55e97120ff1cfa2b8add5793f0415615bf
Magnun A V F <magnunavf@gmail.com>
2023-06-08 17:59:15 -0300

    remove env vars lib

--- main.go
+++ main.go
@@ -7,15 +7,9 @@ import (
 	"github.com/MagnunAVF/tasks-api/db"
 	"github.com/MagnunAVF/tasks-api/handlers"
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
 	if os.Getenv("ENV") == "prod" {
 		gin.SetMode(gin.ReleaseMode)
 	}
